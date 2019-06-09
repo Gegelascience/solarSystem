4dd878dda99262039e2a9b6b21c4105efea699fd
gegelascience <remi.verchere2@gmail.com>
2019-06-09 15:29:02 +0200

    add planets obliquity

--- planet.js
+++ planet.js
@@ -2,8 +2,7 @@ AFRAME.registerComponent('planet', {
   schema: {
     tRevol: { type: 'number', default: 500000 },
     semiAxe: { type: 'number', default: 20 },
-    angularStart: { type: 'number', default: 0 },
-    tRotation: { type: 'number', default: 5000 }
+    angularStart: { type: 'number', default: 0 }
   },
   init: function () {
 
@@ -15,9 +14,6 @@ AFRAME.registerComponent('planet', {
     pos.x = 2 * this.data.semiAxe * Math.cos(this.data.angularStart + 360 * (time + timeDelta) / this.data.tRevol);
     pos.z = 2 * this.data.semiAxe * Math.sin(this.data.angularStart + 360 * (time + timeDelta) / this.data.tRevol);
     this.el.setAttribute("position", pos);
-    var rot = this.el.getAttribute("rotation");
-    rot.y = 360 * (time + timeDelta) / this.data.tRotation;
-    this.el.setAttribute("rotation", rot);
   }
 
 })
